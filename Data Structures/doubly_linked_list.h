28e307cf4d2f23c260730ca1cf6b074ea88fdaff
Gabriel Borges <soliid002@gmail.com>
2024-06-29 23:42:37 -0400

    Added Doubly Linked Lists

---
+++ Data Structures/doubly_linked_list.h
@@ -0,0 +1,24 @@
+#ifndef _doubly_linked_list_h_
+#define _doubly_linked_list_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TRUE 1
+#define FALSE 0
+
+// Doubly Linked List
+typedef struct node {
+  int data;
+  struct node *prev;
+  struct node *next;
+} List;
+
+/* * Functions * */
+List* create();
+void print(List *l);
+void destroy(List *l);
+
+
+#endif
--- Data Structures/singly_linked_list.h
+++ Data Structures/singly_linked_list.h
@@ -14,14 +14,31 @@ typedef struct node {
   struct node *next;
 } List;
 
-/* */
+
+/* * Functions * */
+
+/* Creates a singly linked list.*/
 List* create ();
+
+/* Prints a linked list. O(n)*/
 void print (List *l);
+
+/* Frees the memory of a linked list. O(n)*/
 void destroy (List *l);
+
+/* Inserts a new element at the front of a linked list. O(1)*/
 List* insert_front (List *l, int val);
+
+/* Inserts a new element at the end of a linked list. O(n)*/
 List* insert_back (List *l, int val);
+
+/* Removes the first element with value 'val'. O(n)*/
 List* remove_value (List *l, int val);
+
+/* Removes the element at k-th position. O(k)*/
 List* remove_k (List *l, int k);
+
+/* Searches a value in a linked list. O(n)*/
 List* search (List *l, int val);
 
 #endif
