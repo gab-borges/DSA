69de451f805beed0d802e145e71332f947e4e98b
Gabriel Borges <soliid002@gmail.com>
2024-07-02 13:37:03 -0400

    Added Adjacency List representation

---
+++ Data Structures/Graph/adj_list.cpp
@@ -0,0 +1,43 @@
+
+/*
+	*	Implementation of a directed weighted Graph using the
+	*	Adjacency List representation.
+	*
+	*	Considering 'V' as the number of vertices, and 'E' as 
+  * the number of edges, the complexities to initialize the
+  * graph are:
+	*	Time Complexity: O(V+E)
+	*	Space Complexity: O(V+E) 
+*/
+
+#include <iostream>
+#include <list>
+#include <map>
+using namespace std;
+
+
+class Graph {
+  private:
+    int num_vertices;
+    map<int, list<pair<int, int> > > adj_list;
+
+	public:
+		// Adds an edge with weight 'w', from 'u' to 'v'
+		void add_edge(int u, int v, int w) {
+			pair<int, int> p(v, w);
+			
+			adj_list[u].push_back(p);
+		}
+
+		// Prints the adjacency list
+		void print() {
+			for (auto i : adj_list) {
+				cout << i.first << " -> ";
+
+				for (auto j : i.second)
+					cout << j.first << ", " << j.second << "  ";
+
+				cout << endl;
+			}
+		}
+};
