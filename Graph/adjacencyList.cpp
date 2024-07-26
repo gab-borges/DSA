4cef7dc1b31ff6b62023b49682261e52416f68a1
gab-borges <soliid002@gmail.com>
2024-07-25 20:22:20 -0400

    Updated style

--- Graph/adjacencyList.cpp
+++ Graph/adjacencyList.cpp
@@ -1,41 +1,41 @@
-
 /*
-	*	Implementation of a directed weighted Graph using the
-	*	Adjacency List representation.
-	*
-	*	Considering 'V' as the number of vertices, and 'E' as 
-	*	the number of edges, the complexities to initialize the
-	*	graph are:
-	*	Time Complexity: O(V+E)
-	*	Space Complexity: O(V+E) 
-*/
+ * Implementation of a weighted graph using the
+ * adjacency list representation.
+ *
+ * Complexity to initialize the graph:
+ * Time: O(V + E)
+ * Space: O(V + E) 
+ */
 
 #include <iostream>
 #include <list>
 #include <map>
 using namespace std;
 
-
 class Graph {
-  private:
-    int num_vertices;
-    map<int, list<pair<int, int> > > adj_list;
+	int V; // Number of vertices
+	vector<vector<pair<int, int> > > adj_list; // Graph representation
 
-	public:
-		// Adds an edge with weight 'w', from 'u' to 'v'
-		void add_edge(int u, int v, int w) {
-			adj_list[u].push_back({v, w});
-		}
+public:
+	// Adds an edge with weight 'w', from 'u' to 'v'
+	void add_edge(int u, int v, int w);
 
-		// Prints the adjacency list
-		void print() {
-			for (auto i : adj_list) {
-				cout << i.first << " -> ";
-
-				for (auto j : i.second)
-					cout << j.first << ", " << j.second << "  ";
-
-				cout << endl;
-			}
-		}
+	// Prints the adjacency list
+	void print();
 };
+
+void Graph::add_edge(int u, int v, int w) {
+	adj_list[u].push_back({v, w});
+	adj_list[v].push_back({u, w}); // For undirected graphs
+}
+
+void Graph::print() {
+	for (auto i : adj_list) {
+		cout << i.first << " -> ";
+
+		for (auto j : i.second)
+			cout << j.first << ", " << j.second << "  ";
+
+		cout << endl;
+	}
+}
