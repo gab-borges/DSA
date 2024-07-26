41236a43129d7053b0f6a79e099b82f92d25add9
gab-borges <soliid002@gmail.com>
2024-07-25 20:32:18 -0400

    Updated style

--- Graph/adjacencyMatrix.cpp
+++ Graph/adjacencyMatrix.cpp
@@ -1,41 +1,45 @@
-
 /*
-	*	Implementation of a directed weighted Graph using the
-	*	Adjacency Matrix representation.
-	*
-	*	Considering V as the number of vertices, the complexities
-	*	to initialize the graph are:
-	*	Time Complexity: O(V²)
-	*	Space Complexity: O(V²) 
-*/
+ * Implementation of a weighted Graph using the
+ * adjacency matrix representation.
+ *
+ * Complexity to initialize the graph:
+ * Time: O(V²)
+ * Space: O(V²)
+ */
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-
 class Graph {
-	private:
-		vector<vector<int>> adj_matrix;
-		int num_vertices;
+	int V; // Number of vertices
+	vector<vector<int>> adj_matrix; // Graph representation
 
-	public:
-		// Initializes the matrix filled with zeros of a graph with 'n' nodes.
-		Graph(int n) {
-			adj_matrix = vector<vector<int>>(n, vector<int>(n, 0));
-		}
+public:
+	// Constructor that fills the adj_matrix with zeros
+	Graph(int n);
 
-		// Adds an edge with weight 'w', from 'u' to 'v'
-		void add_edge(int u, int v, int w) {
-			adj_matrix[u][v] = w;
-		}
+	// Adds an edge with weight 'w', between 'u' to 'v'
+	void add_edge(int u, int v, int w);
 
-		// Prints the adjacency matrix
-		void print() {
-			for (int i = 0; i < num_vertices; i++) {
-				for (int j = 0; j < num_vertices; j++)
-					cout << adj_matrix[i][j] << " ";
-				cout << endl;
-			}
-		}
+	// Prints the adjacency matrix
+	void print();
 };
+
+Graph::Graph(int n) {
+	adj_matrix = vector<vector<int>>(n, vector<int>(n, 0));
+}
+
+Graph::void add_edge(int u, int v, int w) {
+	adj_matrix[u][v] = w;
+	adj_matrix[v][u] = w; // For undirected graphs
+}
+
+Graph::void print() {
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j < V; j++)
+			cout << adj_matrix[i][j] << " ";
+
+		cout << endl;
+	}
+}
